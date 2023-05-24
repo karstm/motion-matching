dfe52c62bffdd535a0cb2c07f4b6abc02cab2ac1
Pablo-Paniagua <josepa_pahe@hotmail.com>
2023-05-24 20:00:08 +0200

    Added bvh files

--- database_generator/filtering.py
+++ database_generator/filtering.py
@@ -5,6 +5,8 @@ from scipy.interpolate import griddata
 import numpy as np
 import glob
 
+upsample = False
+
 import os
 os.chdir(os.path.dirname(__file__))
 
@@ -12,31 +14,34 @@ os.chdir(os.path.dirname(__file__))
 folder_path = os.path.join(os.path.dirname(__file__), "..", "data", "mocap", "lafan1_selected")
 files = glob.glob(os.path.join(folder_path, "*"))
 
+
+
 def generate_sim_bone(bvh=None):
     # Extract positions and rotations
     positions = bvh['positions']
     rotations = quat.unroll(quat.from_euler(np.radians(bvh['rotations']), order=bvh['order']))
-
     ''' Super sample'''
-    nframes = positions.shape[0]
-    nbones = positions.shape[1]
-    
-    # Supersample data to 60 fps
-    original_times = np.linspace(0, nframes - 1, nframes)
-    sample_times = np.linspace(0, nframes - 1, (nframes * 2 - 1))
+    if upsample:
+        nframes = positions.shape[0]
+        nbones = positions.shape[1]
+        
+        # Supersample data to 60 fps
+        original_times = np.linspace(0, nframes - 1, nframes)
+        sample_times = np.linspace(0, nframes - 1, (nframes * 2 - 1))
 
-    # This does a cubic interpolation of the data for supersampling
-    positions = griddata(original_times, positions.reshape([nframes, -1]), sample_times, method='cubic').reshape([len(sample_times), nbones, 3])
-    rotations = griddata(original_times, rotations.reshape([nframes, -1]), sample_times, method='cubic').reshape([len(sample_times), nbones, 4])
-    
-    # Need to re-normalize after super-sampling
-    rotations = quat.normalize(rotations)
+        # This does a cubic interpolation of the data for supersampling
+        positions = griddata(original_times, positions.reshape([nframes, -1]), sample_times, method='cubic').reshape([len(sample_times), nbones, 3])
+        rotations = griddata(original_times, rotations.reshape([nframes, -1]), sample_times, method='cubic').reshape([len(sample_times), nbones, 4])
+        
+        # Need to re-normalize after super-sampling
+        rotations = quat.normalize(rotations)
 
-    bvh['frametime'] = 1 / 60.0
+        bvh['frametime'] = 1 / 60.0
     
     ''' Simulation bone and filtering '''
     # First compute world space positions/rotations
     global_rotations, global_positions = quat.fk(rotations, positions, bvh['parents'])
+    nframes = positions.shape[0]
     
     # Specify joints to use for simulation bone 
     sim_position_joint = bvh['names'].index("Spine2")
@@ -44,14 +49,14 @@ def generate_sim_bone(bvh=None):
     
     # Position comes from spine joint
     sim_position = np.array([1.0, 0.0, 1.0]) * global_positions[:,sim_position_joint:sim_position_joint+1]
-    sim_position = signal.savgol_filter(sim_position, 31, 3, axis=0, mode='interp')
+    sim_position = signal.savgol_filter(sim_position, min(31, nframes), 3, axis=0, mode='interp')
     
     # Direction comes from projected hip forward direction
     sim_direction = np.array([1.0, 0.0, 1.0]) * quat.mul_vec(global_rotations[:,sim_rotation_joint:sim_rotation_joint+1], np.array([0.0, 1.0, 0.0]))
 
     # We need to re-normalize the direction after both projection and smoothing
     sim_direction = sim_direction / np.sqrt(np.sum(np.square(sim_direction), axis=-1))[...,np.newaxis]
-    sim_direction = signal.savgol_filter(sim_direction, 61, 3, axis=0, mode='interp')
+    sim_direction = signal.savgol_filter(sim_direction, min(61, nframes), 3, axis=0, mode='interp')
     sim_direction = sim_direction / np.sqrt(np.sum(np.square(sim_direction), axis=-1)[...,np.newaxis])
     
     # Extract rotation from direction
@@ -80,7 +85,6 @@ def generate_sim_bone(bvh=None):
 
 
 """ Loop Over Files """
-# save_path = os.path.join(os.path.dirname(__file__), "..", "data", "mocap", "lafan1_filtered")
 for filename in files:
     if ('filtered' in filename): continue
     print('Loading "%s" ...' % (filename))
